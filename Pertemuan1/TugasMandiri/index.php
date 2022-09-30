0f5170c9a0872c301c239ed80622b13c2ac456d5
edorival06 <kolothjr@gmail.com>
2022-09-30 08:56:04 +0700

    index

--- Pertemuan1/TugasMandiri/index.php
+++ Pertemuan1/TugasMandiri/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Latihan Require</title>
+    <title>Include</title>
 </head>
 <body>
     <?php
         include "header.php";
         include "navigasi.php";
-        include "footer.php";
+        include "konten.php";
     ?>
 </body>
 </html>
